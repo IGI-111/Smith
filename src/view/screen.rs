d1aed615f173cc0d7fe70dbdcc5a2b1e2c696132
IGI-111 <igi-111@protonmail.com>
2020-07-08 20:14:13 +0200

    Fix up double buffering

--- src/view/screen.rs
+++ src/view/screen.rs
@@ -1,21 +1,19 @@
 use ndarray::{Array, Array2};
-use std;
 use std::cell::RefCell;
 use std::fmt::Write as FmtWrite;
 use std::io;
 use std::io::{BufWriter, Write};
 use std::iter::FromIterator;
 use syntect::highlighting::Style;
-use termion;
 use termion::color;
 use termion::input::MouseTerminal;
 use termion::raw::{IntoRawMode, RawTerminal};
 use termion::screen::AlternateScreen;
-use termion::style;
 
 pub struct Screen {
-    out: RefCell<MouseTerminal<AlternateScreen<RawTerminal<BufWriter<io::Stdout>>>>>,
-    buf: RefCell<Array2<(Style, char)>>,
+    out: RefCell<RawTerminal<MouseTerminal<AlternateScreen<BufWriter<io::Stdout>>>>>,
+    write_buf: RefCell<Array2<(Style, char)>>,
+    read_buf: RefCell<Array2<(Style, char)>>,
     cursor_pos: (usize, usize),
     cursor_visible: bool,
     default_style: Style,
@@ -24,16 +22,24 @@ pub struct Screen {
 impl Screen {
     pub fn with_default_style(default_style: Style) -> Self {
         let (w, h) = termion::terminal_size().unwrap();
-        let buf =
+        let write_buf =
             Array::from_iter(std::iter::repeat((default_style, ' ')).take(w as usize * h as usize));
-        let buf = buf.into_shape((h as usize, w as usize)).unwrap();
+        let write_buf = write_buf.into_shape((h as usize, w as usize)).unwrap();
+        let read_buf =
+            Array::from_iter(std::iter::repeat((default_style, 'X')).take(w as usize * h as usize));
+        let read_buf = read_buf.into_shape((h as usize, w as usize)).unwrap();
+        let out = RefCell::new(
+            MouseTerminal::from(AlternateScreen::from(BufWriter::with_capacity(
+                1 << 14,
+                io::stdout(),
+            )))
+            .into_raw_mode()
+            .unwrap(),
+        );
         Screen {
-            out: RefCell::new(MouseTerminal::from(AlternateScreen::from(
-                BufWriter::with_capacity(1 << 14, io::stdout())
-                    .into_raw_mode()
-                    .unwrap(),
-            ))),
-            buf: RefCell::new(buf),
+            out,
+            read_buf: RefCell::new(read_buf),
+            write_buf: RefCell::new(write_buf),
             cursor_pos: (0, 0),
             cursor_visible: true,
             default_style,
@@ -41,29 +47,39 @@ impl Screen {
     }
 
     pub fn clear(&self) {
-        for cell in self.buf.borrow_mut().iter_mut() {
+        for cell in self.write_buf.borrow_mut().iter_mut() {
             *cell = (self.default_style, ' ');
         }
     }
 
     pub fn present(&self) {
         let mut out = self.out.borrow_mut();
-        let buf = self.buf.borrow();
+        let mut read_buf = self.read_buf.borrow_mut();
+        let write_buf = self.write_buf.borrow();
 
         write!(out, "{}", termion::cursor::Hide).unwrap();
         let mut last_style = self.default_style;
         write!(out, "{}", Self::escape_style(&last_style)).unwrap();
 
-        let (h, w) = buf.dim();
+        let (h, w) = write_buf.dim();
         for y in 0..h {
-            write!(out, "{}", termion::cursor::Goto(1, 1 + y as u16)).unwrap();
             for x in 0..w {
-                let (style, ref text) = buf[[y, x]];
-                if style != last_style {
-                    write!(out, "{}{}", style::Reset, Self::escape_style(&style)).unwrap();
-                    last_style = style;
+                if write_buf[[y, x]] != read_buf[[y, x]] {
+                    read_buf[[y, x]] = write_buf[[y, x]];
+
+                    let (style, ref text) = write_buf[[y, x]];
+                    if style != last_style {
+                        write!(out, "{}", Self::escape_style(&style)).unwrap();
+                        last_style = style;
+                    }
+                    write!(
+                        out,
+                        "{}{}",
+                        termion::cursor::Goto(1 + x as u16, 1 + y as u16),
+                        text
+                    )
+                    .unwrap();
                 }
-                write!(out, "{}", text).unwrap();
             }
         }
 
@@ -90,8 +106,8 @@ impl Screen {
     }
 
     pub fn draw_ranges(&self, x: usize, y: usize, ranges: Vec<(Style, &str)>) {
-        let mut buf = self.buf.borrow_mut();
-        let (h, w) = buf.dim();
+        let mut write_buf = self.write_buf.borrow_mut();
+        let (h, w) = write_buf.dim();
         if y >= h {
             return;
         }
@@ -101,7 +117,7 @@ impl Screen {
                 if x >= w {
                     break;
                 }
-                buf[[y, x]] = (style, g);
+                write_buf[[y, x]] = (style, g);
                 x += 1;
             }
         }
